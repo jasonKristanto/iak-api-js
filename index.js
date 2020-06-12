14b8af40ad4d0ad4ed3d11b3020cb0814921e8f0
gandhi <gandhi.pranata@student.umn.ac.id>
2020-06-12 15:41:41 +0700

    remove postpaid in index.js

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 // ES6
 import {checkBalance, pricelist} from "./library/prepaid";
 
-import {checkstatus, inquiry, payment, pricelist as pricelistpasca, receipt} from "./library/postpaid";
-
 (async () => {
   const checkBalanceResponse = await checkBalance(
     "081807971414",
@@ -18,71 +16,5 @@ import {checkstatus, inquiry, payment, pricelist as pricelistpasca, receipt} fro
   );
   console.log("Pricelist");
   console.log(pricelistResponse);
-
-  const pricelistPascaResponse = await pricelistpasca(
-      "sandbox",
-      "kudo",
-      "6155c989ff465120",
-      "all"
-  );
-  console.log("Pricelist Pasca");
-  console.log(pricelistPascaResponse);
-
-  const inquiryPascaResponse = await inquiry(
-      "sandbox",
-      "kudo",
-      "6155c989ff465120",
-      "PBBKOT.CIMAHI",
-      "329801092375999901",
-      "PBB123"
-  );
-  console.log("Inquiry Pasca");
-  console.log(inquiryPascaResponse);
-
-  const payPascaResponse = await payment(
-      "sandbox",
-      "kudo",
-      "6155c989ff465120",
-      "9881240"
-  );
-  console.log("Pay Pasca");
-  console.log(payPascaResponse);
-
-  const checkstatusPascaResponse = await checkstatus(
-      "sandbox",
-      "kudo",
-      "6155c989ff465120",
-      "PBB123"
-  );
-  console.log("Checkstatus Pasca");
-  console.log(checkstatusPascaResponse);
-
-  const receiptPascaResponse = await receipt(
-      "sandbox",
-      "9881240"
-  );
-  console.log("Receipt Pasca");
-  console.log(receiptPascaResponse);
-
-  const pricelistPascaResponseProd = await pricelistpasca(
-      "prod",
-      "kudo",
-      "2895d383c9744242289",
-      "all"
-  );
-  console.log("Pricelist Pasca Prod");
-  console.log(pricelistPascaResponseProd);
-
-  const inquiryPascaResponseProd = await inquiry(
-      "prod",
-      "082210138584",
-      "1231234",
-      "PLNPOSTPAID",
-      "543400746892",
-      "PLN123"
-  );
-  console.log("Inquiry Pasca Prod");
-  console.log(inquiryPascaResponseProd);
-
 })();
 // module.exports = IAK;
