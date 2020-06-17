0b954b084ee24aafa42f56a29ecbaa4967dce01c
gandhipranata <40458510+gandhipranata@users.noreply.github.com>
2020-06-17 15:24:48 +0700

    init postpaid (#1)
    Initialize Postpaid Library

--- config/config.js
+++ config/config.js
@@ -1,4 +1,5 @@
 export const endpoint = {
   PREPAID_ENDPOINT: "https://prepaid.iak.dev/api/",
-  POSTPAID_ENDPOINT: ""
+  POSTPAID_SANDBOX_ENDPOINT: "https://testpostpaid.mobilepulsa.net/",
+  POSTPAID_PROD_ENDPOINT: "https://mobilepulsa.net/"
 };
