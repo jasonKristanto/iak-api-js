673082349c1d1fb5ac85bca44065e76ddcd1a9f0
Martha Saphira <msaphira17@gmail.com>
2020-07-08 15:56:03 +0700

    Remove .dist folder and node module

--- dist/config/config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var endpoint = exports.endpoint = {
-  PREPAID_ENDPOINT: "https://prepaid.iak.dev/api/",
-  POSTPAID_ENDPOINT: ""
-};
